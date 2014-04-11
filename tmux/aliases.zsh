1cd503ef30147698b776a845d14ca0b219e351e3
viccuad <me@viccuad.me>
2014-04-11 04:23:14 +0200

    added oh-my-zsh tmux plugin

---
+++ tmux/aliases.zsh
@@ -0,0 +1,5 @@
+#tmux Aliases
+
+alias tmuxa='tmux attach -t'
+alias tmuxs='tmux new-session -s'
+alias tmuxl='tmux list-sessions'
---
+++ tmux/completion.zsh
@@ -0,0 +1,2 @@
+# Use the completions for tmux for our function
+compdef _tmux _zsh_tmux_plugin_run
