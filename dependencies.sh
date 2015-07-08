af0113795e75801e9293c1e7a209e20f449c7fca
viccuad <me@viccuad.me>
2015-07-08 18:12:23 +0200

    dependencies: updated, morfed into a .sh

---
+++ dependencies.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# for tmux line:
+# rainbarf  # from github, needs compiling. Right now the bin is in bin.nostow
+
+# tools:
+sudo apt-get install vim nvim tmux git ack-grep tig git-gui picocom mutt weechat
+
+# autocompletion/syntax:
+sudo apt-get install python-jedi python3-jedi exuberant-ctags build-essential cmake python-dev
+
+# for arduino development:
+pip install --user inotool
