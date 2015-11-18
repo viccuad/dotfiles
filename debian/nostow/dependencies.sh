02a204e4a7a86f38d99af4a3eb7f998bec2cea83
viccuad <me@viccuad.me>
2015-11-18 21:25:20 +0100

    Update debian dependencies.sh

--- debian/nostow/dependencies.sh
+++ debian/nostow/dependencies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Debian packages:
-sudo apt install autoconf automake autotools-dev debhelper dh-make devscripts fakeroot patch patchutils pbuilder quilt xutils-dev debbindiff lintian dupload mc git-buildpackage
+sudo apt install reportbug apt-listbugs apt-listchanges apt-mark how-can-i-help gdebi autoconf automake autotools-dev debhelper dh-make  dh-autoreconf debian-goodies debian-archive-keyring debianutils debootstrap deborphan devscripts fakeroot patch patchutils pbuilder quilt xutils-dev debbindiff lintian dupload mc git-buildpackage
