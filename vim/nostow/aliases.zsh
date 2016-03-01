b45b400fe776f576e98ba12dc9bcb80ab49bc70e
Víctor Cuadrado Juan <me@viccuad.me>
2016-03-01 20:19:40 +0100

    Vim: trim aliases

--- vim/nostow/aliases.zsh
+++ vim/nostow/aliases.zsh
@@ -1,6 +1,3 @@
-# Open last modified file in vim
-alias Vim="vim `ls -t | head -1`"
-
 # Open a server instance of vim 
 alias vi="vim --servername VIM"
 alias vim="vim --servername VIM"
@@ -10,12 +7,3 @@ alias vimmin="vim -u ~/.vimrc_minimal"
 
 # only have 1 open gvim window
 gvim() { command gvim -p --remote-silent "$@" || command gvim -p "$@"; }
-
-# for vim-superman plugin for man
-vman() {
-		vim -c "SuperMan $*"
-
-		if [ "$?" != "0" ]; then
-				echo "No manual entry for $*"
-		fi
-}
