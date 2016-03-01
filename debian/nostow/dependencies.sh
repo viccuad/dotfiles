e9792452c4b1764af5cdcbafd89970ee8a733f7c
Víctor Cuadrado Juan <me@viccuad.me>
2016-03-01 20:12:40 +0100

    Debian: update dependencies

--- debian/nostow/dependencies.sh
+++ debian/nostow/dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Debian packages:
-sudo apt install reportbug apt-listbugs apt-listchanges apt-mark how-can-i-help gdebi autoconf automake autotools-dev debhelper dh-make dh-autoreconf debian-goodies debian-archive-keyring debianutils debootstrap deborphan devscripts fakeroot patch patchutils pbuilder quilt xutils-dev debbindiff lintian dupload mc git-buildpackage dput
+sudo apt install reportbug apt-listbugs apt-listchanges apt-mark how-can-i-help gdebi autoconf automake autotools-dev debhelper dh-make dh-autoreconf debian-goodies debian-archive-keyring debianutils debootstrap deborphan devscripts fakeroot patch patchutils pbuilder quilt xutils-dev debbindiff lintian dupload mc git-buildpackage dput git-dpm
 
 # python packages:
 sudo apt install python-all-dev python-stdeb python3-all-dev python3-stdeb
