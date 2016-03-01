042b8069f03afd167c75f0b5812917a321c36df8
Víctor Cuadrado Juan <me@viccuad.me>
2016-03-01 20:19:27 +0100

    Vim: update dependencies

--- vim/nostow/dependencies.sh
+++ vim/nostow/dependencies.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # debian package:
-sudo apt-get install vim vim-gnome python-jedi python3-jedi exuberant-ctags build-essential cmake python-dev
+sudo apt-get install vim vim-gnome python-jedi python3-jedi exuberant-ctags build-essential cmake python-dev silversearcher-ag
 
 # for youcompleteme c support, when building clang:
 # build-essential cmake python-dev  
+
+# for having en locale installed, to be used inside of vim:
+sudo dpkg-reconfigure locales
