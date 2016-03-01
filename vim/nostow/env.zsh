f3f72425ecb802accedd21367f131cfd8247551e
Víctor Cuadrado Juan <me@viccuad.me>
2016-03-01 20:20:02 +0100

    Vim: use as $MANPAGER

--- vim/nostow/env.zsh
+++ vim/nostow/env.zsh
@@ -1,3 +1,4 @@
 # use a minimal vim with no plugins for sudoedit:
 export SUDO_EDITOR="vim -u ~/.vimrc_minimal"
 
+export MANPAGER="/bin/sh -c \"col -b | vim -c 'set ft=man ts=8 nomod nolist nonu noma' -\""
