ae7247d8edc1c2b61e18b3a7ae1f36c73f200576
Víctor Cuadrado Juan <me@viccuad.me>
2016-03-30 23:23:46 +0200

    Zsh: Fix hiccup on vi-mode when doing Alt-j or Alt-k

--- zsh/nostow/vim_mode.zsh
+++ zsh/nostow/vim_mode.zsh
@@ -2,7 +2,7 @@
 bindkey -v
 
 # remap vim Esc key to jk
-bindkey -M viins 'jk' vi-cmd-mode # NOTE: setting KEYTIMEOUT too low prohibits zle from recognizing double press
+bindkey -M viins 'jk' vi-cmd-mode
 
 # allow v to edit the command line (standard behaviour)
 autoload -Uz edit-command-line
@@ -10,10 +10,14 @@ bindkey -M vicmd 'v' edit-command-line
 # allow ctrl-p, ctrl-n for navigate history (standard behaviour)
 bindkey '^P' up-history
 bindkey '^N' down-history
+# allow alt-j, alt-k for navigating history without entering vi mode
+bindkey '^[k' up-history
+bindkey '^[j' down-history
 # allow ctrl-h, ctrl-w, ctrl-? for char and word deletion (standard behaviour)
 bindkey '^?' backward-delete-char
 bindkey '^h' backward-delete-char
 bindkey '^w' backward-kill-word
 
 # make mode changes instantenous, 10ms for key sequences:
+# NOTE: setting KEYTIMEOUT too low prohibits zle from recognizing double press
 export KEYTIMEOUT=10
