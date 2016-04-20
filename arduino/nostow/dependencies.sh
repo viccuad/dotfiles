2d69ff0f7c670a71c9e67eb34f9ae8d7d8c0bf45
Víctor Cuadrado Juan <me@viccuad.me>
2016-04-20 19:36:14 +0200

    Arduino: update dependencies, add ino.ino

--- arduino/nostow/dependencies.sh
+++ arduino/nostow/dependencies.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
 # debian package:
-pip install --user inotool;
+pip install --user ino;
 sudo apt-get install picocom
+
+# add user to tty and dialout groups:
+sudo usermod -a -G tty $LOGNAME
+sudo usermod -a -G dialout $LOGNAME
+
+echo "Log off and log on again for the changes to take effect!"
