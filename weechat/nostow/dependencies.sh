ffd91cb0f7f195c85c45aa6f1301043f9e66dac8
Víctor Cuadrado Juan <me@viccuad.me>
2016-06-04 00:12:05 +0200

    Weechat: Update configs, dependencies

--- weechat/nostow/dependencies.sh
+++ weechat/nostow/dependencies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # debian package:
-sudo apt-get install weechat weechat-plugins weechat-scripts lua-cjson
+sudo apt-get install weechat weechat-plugins weechat-scripts lua-cjson libnotify-bin
