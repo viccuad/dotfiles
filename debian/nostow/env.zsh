d181852f3463b8bf7a1be9d938ece4afae76ad25
Víctor Cuadrado Juan <me@viccuad.me>
2016-06-04 00:02:33 +0200

    Debian: update env vars

--- debian/nostow/env.zsh
+++ debian/nostow/env.zsh
@@ -1,2 +1,3 @@
-# export DEBFULLNAME="Your full name"
-# export DEBEMAIL="Your mail"
+export DEBFULLNAME="Víctor Cuadrado Juan"
+export DEBEMAIL="me@viccuad.me"
+export DEB_BUILD_OPTIONS="parallel=3" # 4, 8… depending on your computer
