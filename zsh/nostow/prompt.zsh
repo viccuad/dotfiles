f9a6462222e897396125b17d404e28c1e065a697
Víctor Cuadrado Juan <me@viccuad.me>
2016-06-03 23:59:28 +0200

    Zsh: refactor prompt.zsh

--- zsh/nostow/prompt.zsh
+++ zsh/nostow/prompt.zsh
@@ -8,7 +8,7 @@ export GIT_PS1_SHOWSTASHSTATE="true"
 export GIT_PS1_SHOWUNTRACKEDFILES="true"
 export GIT_PS1_SHOWUPSTREAM="auto verbose"
 export GIT_PS1_DESCRIBE_STYLE="branch"
-export GIT_PS1_SHOWCOLORHINTS="true"
+export GIT_PS1_SHOWCOLORHINTS="true" # only available when using precmd()
 export GIT_PS1_HIDE_IF_PWD_IGNORED="true"
 
 ## TODO.txt prompt:
@@ -17,25 +17,25 @@ export GIT_PS1_HIDE_IF_PWD_IGNORED="true"
 # command line. I filter it to only count those tagged as "next", so it's more
 # of a motivation to clear out the list. $1 = @next
 todo_count(){
-		if (( $+commands[todo.sh] )) then
-				num=$(echo $(todo.sh ls $1 | awk -v pattern="$1" '$0 ~ pattern && !/ x /{count++} END {print count}'))
-				echo $num
-		else
-				echo 0
-		fi
+    if (( $+commands[todo.sh] )) then
+       num=$(echo $(todo.sh ls $1 | awk -v pattern="$1" '$0 ~ pattern && !/ x /{count++} END {print count}'))
+       echo $num
+       else
+           echo 0
+    fi
 }
 
 function todo_prompt() {
-		local COUNT=$(todo_count $1);
-		if [ -z $COUNT ]; then
-				echo "";
-		else
-				echo "$1: $COUNT";
-		fi
+    local COUNT=$(todo_count $1);
+    if [ -z $COUNT ]; then
+        echo "";
+    else
+        echo "$1: $COUNT";
+    fi
 }
 
 rprompt_todo() {
-echo  "%{$fg[white]%}$(todo_prompt @next)%{$reset_color%}"
+    echo  "%{$fg[white]%}$(todo_prompt @next)%{$reset_color%}"
 }
 
 # function notes_count() {
@@ -61,10 +61,11 @@ echo  "%{$fg[white]%}$(todo_prompt @next)%{$reset_color%}"
 
 function zle-keymap-select zle-line-init
 {
-		# RPROMPT=""
-		RPROMPT=" $(rprompt_todo) $(rprompt_time)"
-		[[ $KEYMAP = vicmd ]] && RPROMPT="%{$fg_bold[white]%}--NORMAL--%{$reset_color%}"
-		zle reset-prompt
+    # RPROMPT=""
+    # RPROMPT=" $(rprompt_todo) $(rprompt_time)"
+    RPROMPT=" $(rprompt_time)"
+    [[ $KEYMAP = vicmd ]] && RPROMPT="%{$fg_bold[white]%}--NORMAL--%{$reset_color%}"
+    zle reset-prompt
 }
 zle -N zle-keymap-select
 zle -N zle-line-init
@@ -72,29 +73,30 @@ zle -N zle-line-init
 # PROMPT build:
 
 prompt_pwd() {
-		echo "%{$fg_bold[white]%}%~%{$reset_color%}"
+    echo "%{$fg_bold[white]%}%~%{$reset_color%}"
 }
 
 prompt_names() {
-		echo "%{$fg_bold[yellow]%}%n@%M%{$reset_color%}"
+    echo "%{$fg_bold[yellow]%}%n@%M%{$reset_color%}"
 }
 
 prompt_symbol() {
-		echo "%{$fg_bold[white]%}$%{$reset_color%}"
+    echo "%{$fg_bold[white]%}$%{$reset_color%}"
 }
 prompt_last_status() {
-		echo "%(?..%{$fg_bold[red]%} %?% %{$reset_color%})"
+    echo "%(?..%{$fg_bold[red]%} %?% %{$reset_color%})"
 }
 
 rprompt_time() {
-	echo "%{$fg[white]%}%T%{$reset_color%}"
+    echo "%{$fg[white]%}%T%{$reset_color%}"
 }
 
 
 
 precmd() {
-		# set_prompt
-		__git_ps1 "$(prompt_names)$(prompt_last_status) $(prompt_pwd)" "$(prompt_symbol) "
-		# RPROMPT="$(rprompt_todo) $(rprompt_time)"
-		RPROMPT="$(rprompt_time)"
+    # set_prompt
+    # __git_ps1 usage: __git_ps1 pre_git_prompt post_git_prompt
+    __git_ps1 "$(prompt_names)$(prompt_last_status) $(prompt_pwd)" "$(prompt_symbol) "
+    # RPROMPT="$(rprompt_todo) $(rprompt_time)"
+    RPROMPT="$(rprompt_time)"
 }
