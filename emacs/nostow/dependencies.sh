6772fcd00f587fb9d36c82f579df8152e8030025
Víctor Cuadrado Juan <me@viccuad.me>
2016-08-26 21:34:02 +0200

    Emacs: Update dependencies

--- emacs/nostow/dependencies.sh
+++ emacs/nostow/dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # debian package:
-sudo apt-get install emacs24 emacs-goodies-el markdown xclip cscope #missing python-pyscope
+sudo apt-get install emacs24 emacs-goodies-el markdown xclip cscope shellcheck #missing python-pyscope
 
 # for gnus:
 sudo apt-get install gnutls openssl w3m
@@ -14,3 +14,9 @@ sudo zypper in emacs cscope pycscope
 
 # other things:
 sudo apt-get install ditaa
+
+# for python layer:
+# sudo apt-get install # missing package: autoflake
+
+# opensuse packages:
+sudo zypper in emacs cscope pycscope
