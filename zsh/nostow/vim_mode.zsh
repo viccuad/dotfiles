35b2133ad23b1063f4f93bea8b4bc370aece151b
Víctor Cuadrado Juan <me@viccuad.me>
2016-10-25 18:01:25 +0200

    Zsh: fix readline shortcuts

--- zsh/nostow/vim_mode.zsh
+++ zsh/nostow/vim_mode.zsh
@@ -5,7 +5,8 @@ bindkey -v
 bindkey -M viins 'jk' vi-cmd-mode
 
 # allow v to edit the command line (standard behaviour)
-autoload -Uz edit-command-line
+autoload -z edit-command-line
+zle -N edit-command-line
 bindkey -M vicmd 'v' edit-command-line
 # allow ctrl-p, ctrl-n for navigate history (standard behaviour)
 bindkey '^P' up-history
@@ -17,6 +18,8 @@ bindkey '^[j' down-history
 bindkey '^?' backward-delete-char
 bindkey '^h' backward-delete-char
 bindkey '^w' backward-kill-word
+# allow ctrl-a for going to the beginning of the line (standart behaviour)
+bindkey '^A' beginning-of-line
 
 # make mode changes instantenous, 10ms for key sequences:
 # NOTE: setting KEYTIMEOUT too low prohibits zle from recognizing double press
